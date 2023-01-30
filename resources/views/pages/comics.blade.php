fc383f314ac9b3edc8e75ded596a9f0a31916349
albyschianni <alberto.schianni@gmail.com>
2023-01-30 16:35:35 +0100

    creazione file e collegamento due pagine

---
+++ resources/views/pages/comics.blade.php
@@ -0,0 +1,5 @@
+@extends('layout.main-layout')
+
+@section('content')
+    <h1>dc comics dettaglio</h1>
+@endsection
---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,5 @@
+@extends('layout.main-layout')
+
+@section('content')
+    <h1>dc comics</h1>
+@endsection
